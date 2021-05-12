dbc9457ec1d815bf85b3ba6bd55971e13aed4272
sayli <sayliwalkoli27@gmail.com>
2021-05-12 17:09:02 +0530

    my code

---
+++ src/samplepackage/HallowWorld.java
@@ -0,0 +1,10 @@
+package samplepackage;
+
+public class HallowWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("hellow world");
+	}
+
+}
